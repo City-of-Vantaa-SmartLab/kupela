d26292a8b9ab8b94e75d43371142c0d8b87eaa2b
Santeri Saarinen <santeri.saarinen@staff.uta.fi>
2017-05-15 09:37:57 +0300

    Add files via upload

---
+++ babel-server.js
@@ -0,0 +1,3 @@
+//Set up babel-register to compiles files on the fly
+require('babel-register');
+require('./server.js');
---
+++ server.js
@@ -0,0 +1,37 @@
+'use strict';
+
+//Import Express module
+var express = require('express');
+//Import path module (packaged with Node.js)
+var path = require('path');
+//Import fs-extra
+var fse = require('fs-extra');
+
+//Import express-session
+var session = require('express-session');
+
+//Create new instance of Express
+var app = express();
+
+//Import server side file for avatar app
+var kupelaApp = require('./kupela_backend');
+
+//Session setup
+app.use(session({secret: 'secret', key: 'express.sid'}));
+
+//Serve static html page from public directory
+app.use('/', express.static(path.join(__dirname, 'public')));
+
+//Set response to all GET calls
+app.get('/', function(req,res){
+  
+
+});
+
+//Set up http server and listen to port 8080
+var server = require('http').createServer(app).listen(process.env.PORT || '8080', function(){
+  console.log('App listening on port %s', server.address().port);
+  console.log('Press Ctrl+C to quit');
+})
+
+kupelaApp.listen(server);
