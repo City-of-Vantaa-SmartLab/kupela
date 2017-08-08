37cd266a52d07b7b54baca7c2287f99b3285c0c3
Tapio <tapix92@gmail.com>
2017-08-08 13:55:40 +0300

    Issue #64: Removed duplicate containers, css not done. Issue #55: Removed old fire location page's contents (Palon sijainti) and replaced with floor plans for each floor. Pictures in the sprinklers reusable replaced with text only.

--- public/components/tehtPage/tabsComponents/reusables/Sprinklerit.js
+++ public/components/tehtPage/tabsComponents/reusables/Sprinklerit.js
@@ -3,11 +3,13 @@ import Showcase from './templates/Showcase';
 
 const Sprinklerit = (props) => (
     <div className="sprinklerit">
-        <p><b>Sprinklerit:</b></p>
-        {props.sprinklers.map((s) =>
-            <a onClick={props.selectSubItem(s.nameId, s, 'yesLayer', 'image')}>
-                <Showcase src={s.url} />
-            </a>
+        <p><b>Kerrokset:</b></p>
+        {props.floorplans.map((s) =>
+            <div>
+                <a onClick={props.selectSubItem(s.nameId, s, 'yesLayer', 'image')}>
+                    <p>{s.name}</p>
+                </a>
+            </div>
         )}
     </div>
 );
