cfaeb5e514d464a64eef6c34ccb02de19c08b8cc
Tapio <tapix92@gmail.com>
2017-08-08 09:29:20 +0300

    Issue #53: Made the selected tab and content defaults set based on the url's 3rd 'parameter', doesn't still work on back/forward buttons, linking works now.

--- public/reducer/tab/content.js
+++ public/reducer/tab/content.js
@@ -1,8 +1,11 @@
 import tabs from '../../JSON/tabs';
 import { SHOW_CURRENT_TABCONTENT } from './actions';
 
-const initialState = tabs[0];
-
+//gets the url, splits it until the parameters needed are left.
+var str = window.location.href;
+var res = str.split("#/");
+res = res[1].split("/");
+const initialState = tabs[tabs.findIndex((item) => item.nameId === res[2])];
 export default (state = initialState, action) => {
     switch (action.type) {
         case SHOW_CURRENT_TABCONTENT:
--- public/reducer/tab/id.js
+++ public/reducer/tab/id.js
@@ -1,12 +1,19 @@
 import { SET_CURRENT_TAB } from './actions';
 import tabs from '../../JSON/tabs';
 
-const initialState = tabs[0].nameId;
 
+//gets the url, splits it until the parameters needed are left.
+var str = window.location.href;
+var res = str.split("#/");
+res = res[1].split("/");
+const initialState = res[2];
 export default (state = initialState, action) => {
     switch (action.type) {
         case SET_CURRENT_TAB:
-            return action.id;
+            var str = window.location.href;
+            var res = str.split("#/");
+            res = res[1].split("/");
+            return res[2];
         default:
             return state;
     }
