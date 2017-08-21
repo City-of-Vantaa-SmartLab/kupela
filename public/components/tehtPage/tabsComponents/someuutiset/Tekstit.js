06e9238f2d30d18437053809f38887cb8b79576e
ktykth92 <gums92@naver.com>
2017-08-21 10:36:51 +0300

    Test page

--- public/components/tehtPage/tabsComponents/someuutiset/Tekstit.js
+++ public/components/tehtPage/tabsComponents/someuutiset/Tekstit.js
@@ -7,15 +7,23 @@ const Tekstit = (props) =>
         <p><b>Tekstit:</b></p>
         {props.texts.map((text) =>
             <div>
-                 <p>Sender name:{text.sender}</p>
-                 <p>Message:{text.message}</p>
-                 <p>Location:{text.location}</p>
-                 <p>Hashtags:{text.hasgtags}</p>
-                 <p>Comments:{text.comments}</p>
+                <a onClick={props.selectItem(text.nameId, 'GET_some', text)}>
+                    <Basic src={text.url} title={text.name} key={text.nameId} />
+                </a>
+                {props.buttons.map((b) =>
+                    {text.nameId===b.id ? text.isShared=b.isShared : false }
+                )}
+                <ShareButton onClick={props.clickShare(text.nameId, text)}
+                             key={text.nameId}
+                             isShared={text.isShared} />
+              <p>Sender name:{text.sender}</p>
+              <p>Message:{text.message}</p>
+              <p>Location:{text.location}</p>
+              <p>Hashtags:{text.hasgtags}</p>
+              <p>Comments:{text.comments}</p>
                  <a onClick={props.selectItem(text.nameId, 'GET_some', text)}>
                      <Basic src={text.url} title={text.name} key={text.nameId} />
                  </a>
-
             </div>
         )}
         {console.log(props)}
