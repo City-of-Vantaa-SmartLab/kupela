f8a7315451fbd8ff8b72724437bfaaa2da4c1d6f
tapix <tapix92@gmail.com>
2017-08-31 14:40:24 +0300

    Added JSON changes removing test data and adding actual data

--- public/JSON/rescuefloorplan.js
+++ public/JSON/rescuefloorplan.js
@@ -3,26 +3,6 @@ const rescuefloorplan = [
         name: 'Sähköpuoli',
         nameId: 'floor1',
         url: '../../../../pics/KUPELA-pohjapiirrokset.png'
-    },
-    {
-        name: 'Floor 2',
-        nameId: 'floor2',
-        url: '../../../../pics/floorplanF2.png'
-    },
-    {
-        name: 'Floor 3',
-        nameId: 'floor3',
-        url: '../../../../pics/floorplanF3.png'
-    },
-    {
-        name: 'Floor 4',
-        nameId: 'floor4',
-        url: '../../../../pics/floorplanF4.png'
-    },
-    {
-        name: 'Floor 5',
-        nameId: 'floor5',
-        url: '../../../../pics/floorplanF5.png'
     }
 ];
 
