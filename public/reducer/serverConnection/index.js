9aeda0f9244941b167469396bb0cf381d175eb87
prossu <santeri.saarinen@staff.uta.fi>
2017-09-01 10:39:58 +0300

    Some changes

--- public/reducer/serverConnection/index.js
+++ public/reducer/serverConnection/index.js
@@ -2,6 +2,7 @@ import { combineReducers } from 'redux';
 import servermessages from './servermessages';
 import dataRequests from './dataRequests';
 import ozCommands from './ozCommands';
+import dataLoaders from './dataLoaders';
 
 export default combineReducers ({
   servermessages,
