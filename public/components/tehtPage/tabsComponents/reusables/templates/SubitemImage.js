bbca54c0633a08160f81c00995d138f8c939d052
tapix <tapix92@gmail.com>
2017-09-07 09:36:43 +0300

    added scrollableArea class to subtitemImages

--- public/components/tehtPage/tabsComponents/reusables/templates/SubitemImage.js
+++ public/components/tehtPage/tabsComponents/reusables/templates/SubitemImage.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const SubItemImage = (props) => (
-    <div className="subitemContent">
+    <div className="subitemContent scrollableArea">
         {props.filter.arr.map((f) =>
             <img key={f.id} src={f.subitem.url}/>
         )}
