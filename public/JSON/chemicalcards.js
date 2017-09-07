678a9bda933b33932eab09e85d7d6c7b57b612a6
tapix <tapix92@gmail.com>
2017-09-07 08:02:59 +0300

    Changed a filepath

--- public/JSON/chemicalcards.js
+++ public/JSON/chemicalcards.js
@@ -2,27 +2,27 @@ const chemicalcards = [
     {
         name: 'Argon',
         nameId: 'kemikaalikortti1',
-        url: '../../../../resources/images/kemikaalikortit/argon_kemikaalikortti.png'
+        url: '../../../../resources/images/kemikaalikortit/argon_kemikaalikortti-1.png'
     },
     {
         name: 'Asetyleeni',
         nameId: 'kemikaalikortti2',
-        url: '../../../../resources/images/kemikaalikortit/asetyleeni_kemikaalikortti.png'
+        url: '../../../../resources/images/kemikaalikortit/asetyleeni_kemikaalikortti-1.png'
     },
     {
         name: 'Happi',
         nameId: 'kemikaalikortti3',
-        url: '../../../../resources/images/kemikaalikortit/happi_kemikaalikortti.png'
+        url: '../../../../resources/images/kemikaalikortit/happi_kemikaalikortti-1.png'
     },
     {
         name: 'Lipeä',
         nameId: 'kemikaalikortti4',
-        url: '../../../../resources/images/kemikaalikortit/lipeä_kemikaalikortti.png'
+        url: '../../../../resources/images/kemikaalikortit/lipea_kemikaalikortti-1.png'
     },
     {
         name: 'Rikkihappo',
         nameId: 'kemikaalikortti5',
-        url: '../../../../resources/images/kemikaalikortit/rikkihappo_kemikaalikortti.png'
+        url: '../../../../resources/images/kemikaalikortit/rikkihappo_kemikaalikortti-1.png'
     },
 ];
 
