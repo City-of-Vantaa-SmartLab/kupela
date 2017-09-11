a4d36e4c3738d65b04b709d9b77fa260a0d8eb8b
prossu <santeri.saarinen@staff.uta.fi>
2017-09-11 12:57:38 +0300

    Changes again

--- public/components/tehtPage/tabsComponents/someuutiset/Uutiset.js
+++ public/components/tehtPage/tabsComponents/someuutiset/Uutiset.js
@@ -6,31 +6,32 @@ import { showAllNews, showPrioNews } from '../../../../reducer/news/actions';
 
 const Uutiset = (props) =>
     <div className="uutiset">
-    <p><b>Kuvat:</b></p>
-    <div className="scrollableArea">
-    {props.news.newsitems.map((newsitem) =>
-        <div className={newsitem.visible === true ? "visible" : "notVisible"}>
-        <div className={newsitem.priority === "1" ? "someNews priority" : "someNews nonpriority"}>
-            <a onClick={props.selectItem(newsitem.nameId, 'GET_some', newsitem)}>
-                <Basic src={newsitem.url} key={newsitem.nameId} />
-            </a>
+      <p><b>Kuvat:</b></p>
+      <input className="showAllNewsButton somebtn" type="button" onClick={props.clickShowAllNews()} value="Näytä kaikki"/>
+      <input className="showPriorityNewsButton somebtn" type="button" onClick={props.clickShowPriorityNews()} value="Näytä tärkeät"/>
 
+      <div className="scrollableArea">
+        {props.news.newsitems.map((newsitem) =>
+            <div className={newsitem.visible === true ? "visible" : "notVisible"}>
+            <div className={newsitem.priority === "1" ? "someNews priority" : "someNews nonpriority"}>
+              <div className="innerSomeText">
+                <a onClick={props.selectItem(newsitem.nameId, 'GET_some', newsitem)}>
+                    <Basic src={newsitem.url} key={newsitem.nameId} />
+                </a>
+              </div>
 
-        {props.buttons.map((b) =>
-            {newsitem.nameId===b.id ? newsitem.isShared=b.isShared : false }
+            {props.buttons.map((b) =>
+                {newsitem.nameId===b.id ? newsitem.isShared=b.isShared : false }
+            )}
+            <ShareButton onClick={props.clickShare(newsitem.nameId, 2, newsitem)}
+                         url={newsitem.url}
+                         key={newsitem.nameId}
+                         isShared={newsitem.isShared} />
+            </div>
+            </div>
         )}
-        <ShareButton onClick={props.clickShare(newsitem.nameId, 2, newsitem)}
-                     url={newsitem.url}
-                     key={newsitem.nameId}
-                     isShared={newsitem.isShared} />
-        </div>
-        </div>
-    )}
-    </div>
-
-    <input className="showAllNewsButton somebtn" type="button" onClick={props.clickShowAllNews()} value="Näytä kaikki"/>
-    <input className="showPriorityNewsButton somebtn" type="button" onClick={props.clickShowPriorityNews()} value="Näytä tärkeät"/>
-</div>;
+      </div>
+    </div>;
 
 const mapDispatchToProps = dispatch => ({
     clickShowAllNews() {
