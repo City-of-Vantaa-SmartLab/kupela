60b621ad5db834fe4914b995b6e0c69f895f5e17
prossu <santeri.saarinen@staff.uta.fi>
2017-09-14 11:01:28 +0300

    Kartat lisätty etusivulle.

--- public/JSON/arrivals.js
+++ public/JSON/arrivals.js
@@ -1,11 +1,11 @@
 const arrivals = [
     {
-        name: 'Saapuminen 1',
+        name: 'Saapuminen 201',
         nameId: 'saapuminen1',
         url: '../../../../resources/images/reittikartta_ekojenreitti.png'
     },
     {
-        name: 'Saapuminen 2',
+        name: 'Saapuminen P30, 301',
         nameId: 'saapuminen2',
         url: '../../../../resources/images/reittikartta_tokienreitti.png'
     }
